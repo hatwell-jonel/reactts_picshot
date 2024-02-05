5185f0aa59ff02f2ad33c4057d7c5c0dca6d26b5
Jonel Hatwell <jonel.hatwell@gmail.com>
2024-02-05 22:28:42 +0800

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import Gallery from './components/Gallery'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Gallery />
+    </>
+  )
+}
+
+export default App
